655afdae950a238c4f0d446cdacaabe461f6103d
Chris Burge <topher.burge@gmail.com>
2019-12-06 15:18:59 -0800

    add style object to form.

--- NewEntryForm.js
+++ NewEntryForm.js
@@ -4,12 +4,19 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function NewEntryForm() {
   return (
     <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <View style={pageStyle.container}>
         <TextInput placeholder="Entry Title" style={{ width: '80%', borderBottomColor: 'black', borderBottomWidth: 1 }} />
         <Button title="ADD" />
       </View>
     </View>
   );
 }
+
+const pageStyle = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+})
 
 
