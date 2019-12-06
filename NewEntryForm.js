8d9254cb4ede6f86653e3a071919c1fd55ec0da0
Chris Burge <topher.burge@gmail.com>
2019-12-06 14:21:43 -0800

    play around with styling on the newentryform file.

--- NewEntryForm.js
+++ NewEntryForm.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function NewEntryForm() {
   return (
-    <View style={{padding: 50}}>
-      <TextInput placeholder="Entry Title" style={{borderBottomColor: 'black', borderBottomWidth: 1}} />
-      <Button title="ADD" />
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TextInput placeholder="Entry Title" style={{ width: '80%', borderBottomColor: 'black', borderBottomWidth: 1 }} />
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
